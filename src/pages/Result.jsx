c62248548f91406decf593300b45d76ff3daea4b
Krishnakant010 <krishnakantgangurde0109@gmail.com>
2023-09-14 10:47:03 +0530

    Home page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { useParams } from "react-router";
 import { Format } from "../components/Format";
+import Navbar from "../components/Navbar";
 
 const Result = ({ lat, long }) => {
   return (
-    <div>
-      The results for the following latitude and longitude are :<p>{lat}</p>
-      <p>{long}</p>
-      <div className="">
-        {" "}
-        <Format />
+    <>
+      <Navbar />
+      <div>
+        The results for the following latitude and longitude are :<p>{lat}</p>
+        <p>{long}</p>
+        <div className="">
+          {" "}
+          <Format />
+        </div>
+        <div></div>
       </div>
-    </div>
+    </>
   );
 };
 
